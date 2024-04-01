72912d2a8bc654bd443b97d51507fb3f8784275c
rsegura8 <areyousure.rey@gmail.com>
2024-03-31 20:43:09 -0500

    fetch employees working

--- controller/employeeController.js
+++ controller/employeeController.js
@@ -1,26 +1,33 @@
-import Employee from "../model/employeeModel.js"
+import Employee from "../model/employeeModel.js";
 
 export const create = async (req, res) => {
-    try {
-        const employeeData = new Employee(req.body)
-        const { userName } = employeeData
-        const employeeExist = await Employee.findOne({userName})
+  try {
+    const employeeData = new Employee(req.body);
+    const { userName } = employeeData;
+    const employeeExist = await Employee.findOne({ userName });
 
-        if(employeeExist) {
-            return res.status(200).json({message: "Employee already exists."})
-        }
-
-        const savedEmployee = await employeeData.save();
-        res.status(200).json(savedEmployee)
-    } catch (error) {
-        res.status(500).json({error: "Internal Server Error"})
+    if (employeeExist) {
+      return res.status(200).json({ message: "Employee already exists." });
     }
-}
 
-export const fetch = async(req, res) => {
-    try {
-        res.json("Hello World")
-    } catch (error) {
-        res.status(500).json({error: "Internal Server Error"})
+    const savedEmployee = await employeeData.save();
+    res.status(200).json(savedEmployee);
+    
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const fetch = async (req, res) => {
+  try {
+    //res.json("Hello World")
+    const employees = await Employee.find();
+    if (employees.length === 0) {
+      return res.status(404).json({ message: "Employees not found!" });
     }
-}
+
+    res.status(200).json(employees);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
